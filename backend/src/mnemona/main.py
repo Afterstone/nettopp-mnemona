5c2e02f0afe4cd81ba22bfefbc3aab5f548e842b
Afterstone <invisiblex@gmail.com>
2023-01-12 15:46:53 +0100

    Fix SPA serving errors

--- backend/src/mnemona/main.py
+++ backend/src/mnemona/main.py
@@ -3,17 +3,17 @@ import json
 
 import httpx
 import uvicorn
-from fastapi import Depends, FastAPI, Request, Response
+from fastapi import Depends, FastAPI, HTTPException, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
-from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
 from .config import (AUTH_SERVER_ENDPOINT, CORS_ORIGINS, HOST, PORT,
                      UVICORN_RELOAD)
 
 # from .database import Session, get_db
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
 
@@ -26,11 +26,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/")
-async def index():
-    return RedirectResponse(url='/index.html')
 
 
 class LoginDetails(BaseModel):
@@ -87,8 +82,19 @@ async def health():
 
         return Response(content=response.text)
 
-app.mount('/', StaticFiles(directory='./dist'), name='ui')
 
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except HTTPException as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
+
+
+app.mount("/", SPAStaticFiles(directory="dist", html=True), name="app")
 
 if __name__ == '__main__':
     uvicorn.run(
