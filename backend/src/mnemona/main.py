84f1b18cc5e4ad4695c6c854fa9d560efa023046
Afterstone <invisiblex@gmail.com>
2023-01-12 16:14:34 +0100

    Fix error with fastapi SPA serving

--- backend/src/mnemona/main.py
+++ backend/src/mnemona/main.py
@@ -3,7 +3,8 @@ import json
 
 import httpx
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, Request, Response
+from fastapi import Depends, FastAPI, Request, Response
+from starlette.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
@@ -87,11 +88,11 @@ class SPAStaticFiles(StaticFiles):
     async def get_response(self, path: str, scope):
         try:
             return await super().get_response(path, scope)
-        except HTTPException as ex:
-            if ex.status_code == 404:
-                return await super().get_response("index.html", scope)
-            else:
-                raise ex
+        except Exception as ex:
+            if isinstance(ex, HTTPException):
+                if ex.status_code == 404:
+                    return await super().get_response("index.html", scope)
+            raise ex
 
 
 app.mount("/", SPAStaticFiles(directory="dist", html=True), name="app")
